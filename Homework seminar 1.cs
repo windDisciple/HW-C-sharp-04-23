8e754c9978ef07f03261af9e953b540d2862dd26
windDisciple <tellittotfishes@gmail.com>
2023-04-26 20:17:49 +0300

    Ready

---
+++ Homework seminar 1.cs
@@ -0,0 +1,52 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// System.Console.WriteLine("Input number 1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number 2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2) {
+//     System.Console.WriteLine($"{num1} > {num2}");
+// }
+// else {
+//     System.Console.WriteLine($"{num2} > {num1}");
+// }
+
+// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// System.Console.WriteLine("Input number 1: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number 2: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input number 3: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1 > num2 && num1 > num3) {
+//     System.Console.WriteLine($"{num1} is max");
+// } if (num2 > num1 && num2 > num3) {
+//     System.Console.WriteLine($"{num2}  is max");
+// } else {
+//     System.Console.WriteLine($"{num3} is max");
+// }
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// System.Console.WriteLine("Input number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 2 == 0) {
+//     Console.WriteLine("Number is even");
+// } else {
+//     Console.WriteLine("Number is odd");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// System.Console.WriteLine("Input number: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// int count = 2;
+// while (count <= n) {
+//     System.Console.WriteLine(count);
+//     count += 2;
+// }
